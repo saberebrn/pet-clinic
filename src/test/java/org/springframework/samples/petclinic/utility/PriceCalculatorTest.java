7b14b933e3242e5b0668f6ad93166674fdbc34ea
Ali Bahari <alibir80@gmail.com>
2021-12-19 18:22:37 +0330

    Test and pitest setup

---
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PriceCalculatorTest {
+
+	@BeforeEach
+	private void setup(){
+
+	}
+
+	@Test
+	public void priceCalcTest(){
+
+	}
+
+	@AfterEach
+	private void teardown(){
+
+	}
+}
